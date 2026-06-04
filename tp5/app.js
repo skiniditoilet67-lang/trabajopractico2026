b55f4e4a0d85d4074f92906a7e621e2cbcdc3653
alumno <51316324@ivt.edu.ar>
2026-06-04 09:07:24 -0300

    4/6

--- tp5/app.js
+++ tp5/app.js
@@ -1,8 +1,34 @@
-let suma=10+10
-alert (suma)
-let resta=5-2
-alert (resta)
-let division=5/5
+//ejercicio suma
+let num1=10
+
+let suma=num1+num1
+alert ("el resultado de la suma es: " + suma)
+//resta
+let num1=5
+let num2=2
+let resta=num1-num2
+alert ("el resultado de la resta: " + resta)
+//division
+let num1=5
+let num2=5
+let division=num1/num2
 console.log ("El resultado de la division es: " + division)
+//multiplicacion
 let multiplicacion=2*2
 console.log ("El resultado de la multiplicación es: " + multiplicacion)
+//alumno
+let nombrealumno= "beni"
+let apellidoalumno= "jorge"
+let edadalumno= 67
+console.log ("el alumno: "+nombrealumno+" "+apellidoalumno+" tiene: " + edadalumno + ' años')
+//precios
+let precio1=67
+let precio2=67
+let precio3=67
+let total= precio1+precio2+precio3
+console.log ("el total a pagar es: " + total)
+//mis cosas favoritas
+let comidafavorita= "capeletinis"
+let colorfavorito= "blanco"
+let numerofavorito= 67
+console.log ("mi comida favorita son los: " + comidafavorita + " mi color favorito es el: " + colorfavorito + " y mi numero favorito es el: " + numerofavorito )
