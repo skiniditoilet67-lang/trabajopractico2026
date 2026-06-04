405bd2b3ec37723316a558049c5da3e4b5654a6f
alumno <51316324@ivt.edu.ar>
2026-06-04 08:06:42 -0300

    tp5

---
+++ tp5/app.js
@@ -0,0 +1,8 @@
+let suma=10+10
+alert (suma)
+let resta=5-2
+alert (resta)
+let division=5/5
+console.log ("El resultado de la division es: " + division)
+let multiplicacion=2*2
+console.log ("El resultado de la multiplicación es: " + multiplicacion)
