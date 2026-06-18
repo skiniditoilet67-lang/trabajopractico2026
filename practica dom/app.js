226a50973b9ed40b46f4e32784d5018a4162e3f6
alumno <51316324@ivt.edu.ar>
2026-06-18 09:06:00 -0300

    18 de Junio

---
+++ practica dom/app.js
@@ -0,0 +1,8 @@
+let titulo = document.querySelector('h1') 
+let boton = document.querySelector('button')
+
+boton.onclick = function () {
+titulo.textContent = 'Buenaaaasssssss'
+titulo.style.color = 'green'
+
+}
